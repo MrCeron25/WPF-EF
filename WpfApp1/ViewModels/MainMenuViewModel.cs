26e4119587b7acc8f8d3d98e5fe57b27643a706b
MrCeron <artemkakrg@gmail.com>
2022-05-14 14:13:24 +0300

    сделал ресурсы для кнопок главное меню

---
+++ WpfApp1/ViewModels/MainMenuViewModel.cs
@@ -0,0 +1,98 @@
+﻿using System.Windows.Input;
+using WpfApp1.ViewModels.Base;
+using WpfApp1.Infrastructure.Commands;
+using System.Windows;
+
+namespace WpfApp1.ViewModels
+{
+    class MainMenuViewModel : ViewModel
+    {
+        #region Вход
+        private string _EntryButtonName = "Вход";
+
+        /// <summary>
+        /// Вход
+        /// </summary>
+        public string EntryButtonName
+        {
+            get => _EntryButtonName;
+            set => Set(ref _EntryButtonName, value);
+        }
+
+        #endregion
+
+        #region Регистрация
+        private string _RegistrationButtonName = "Регистрация";
+
+        /// <summary>
+        /// Регистрация
+        /// </summary>
+        public string RegistrationButtonName
+        {
+            get => _RegistrationButtonName;
+            set => Set(ref _RegistrationButtonName, value);
+        }
+
+        #endregion
+
+        #region Выход
+        private string _ExitButtonName = "Выход";
+
+        /// <summary>
+        /// Выход
+        /// </summary>
+        public string ExitButtonName
+        {
+            get => _ExitButtonName;
+            set => Set(ref _ExitButtonName, value);
+        }
+
+        #endregion
+
+        #region Команды
+
+        public ICommand CloseApplicationCommand { get; }
+
+        private bool CanCloseApplicationCommandExecute(object p) => true;
+
+        private void OnCloseApplicationCommandExecuted(object p)
+        {
+            Application.Current.Shutdown();
+        }
+
+
+
+        public ICommand EntryCommand { get; }
+
+        private bool CanEntryCommandExecute(object p) => true;
+
+        private void OnEntryCommandExecuted(object p)
+        {
+            Manager.Instance.MainFrame.Navigate(new LoginPage());
+        }
+
+
+
+        public ICommand RegistrationCommand { get; }
+
+        private bool CanRegistrationCommandExecute(object p) => true;
+
+        private void OnRegistrationCommandExecuted(object p)
+        {
+            Manager.Instance.MainFrame.Navigate(new RegistrationPage());
+        }
+
+        #endregion
+
+        #region Конструктор
+
+        public MainMenuViewModel()
+        {
+            CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
+            EntryCommand = new LambdaCommand(OnEntryCommandExecuted, CanEntryCommandExecute);
+            RegistrationCommand = new LambdaCommand(OnRegistrationCommandExecuted, CanRegistrationCommandExecute);
+        }
+
+        #endregion
+    }
+}
---
+++ WpfApp1/Views/Pages/MainMenuPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace WpfApp1
+{
+    public partial class MainMenuPage : Page
+    {
+        public MainMenuPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
